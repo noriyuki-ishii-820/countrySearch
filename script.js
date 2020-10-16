c9802d566949c0b8330931a5af655e3cf0e999a4
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-10-16 17:49:39 +1100

    search results

--- script.js
+++ script.js
@@ -11,8 +11,36 @@ searchCountry = () => {
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    const countryName = response[0].name;
-    $("#countryName").text(countryName);
+    $("#countryName").text(response[0].name);
+
+    // overview
+
+    $("#capital").text("Capital : " + response[0].capital);
+    $("#language").text("Language : " + response[0].languages[0].name);
+    $("#population").text(
+      "Population : " + response[0].population.toLocaleString("en")
+    );
+
+    $("#area").text("Area : " + response[0].area.toLocaleString("en") + " km2");
+
+    $("#domain").text("Domain : " + response[0].topLevelDomain);
+    $("#intlphone").text("Calling Code : " + "+" + response[0].callingCodes);
+    $("#timezone").text("Timezone : " + response[0].timezones[0]);
+
+    $("#flag").empty();
+    const flagURL = response[0].flag;
+    const img = $("<img>");
+    img.addClass("flagImg");
+    img.attr("src", flagURL);
+    $("#flag").append(img);
+
+    // geography
+
+    $("#region").text("Region : " + response[0].region);
+    $("#subregion").text("Subregion : " + response[0].subregion);
+    $("#borders").text(
+      "Bordering With : " + response[0].borders.length + " countries."
+    );
 
     const countryLat = response[0].latlng[0];
     const countryLng = response[0].latlng[1];
@@ -29,4 +57,5 @@ searchCountry = () => {
 $("#search").on("click", function (event) {
   event.preventDefault();
   searchCountry();
+  $("userInput").val("");
 });
