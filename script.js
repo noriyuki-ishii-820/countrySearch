b6164f4fbabe2108ff55609284d63de083ddc255
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-10-16 18:07:45 +1100

    polished

--- script.js
+++ script.js
@@ -6,56 +6,62 @@ searchCountry = () => {
 
   const queryURL = "https://restcountries.eu/rest/v2/name/" + userInput;
   console.log(queryURL);
-
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(function (response) {
-    $("#countryName").text(response[0].name);
+  })
+    .fail(function () {
+      alert("the input is invalid, please try again.");
+      location.reload();
+    })
 
-    // overview
+    .then(function (response) {
+      $("#countryName").text(response[0].name);
 
-    $("#capital").text("Capital : " + response[0].capital);
-    $("#language").text("Language : " + response[0].languages[0].name);
-    $("#population").text(
-      "Population : " + response[0].population.toLocaleString("en")
-    );
+      // overview
 
-    $("#area").text("Area : " + response[0].area.toLocaleString("en") + " km2");
+      $("#capital").text("Capital : " + response[0].capital);
+      $("#language").text("Language : " + response[0].languages[0].name);
+      $("#population").text(
+        "Population : " + response[0].population.toLocaleString("en")
+      );
 
-    $("#domain").text("Domain : " + response[0].topLevelDomain);
-    $("#intlphone").text("Calling Code : " + "+" + response[0].callingCodes);
-    $("#timezone").text("Timezone : " + response[0].timezones[0]);
+      $("#area").text(
+        "Area : " + response[0].area.toLocaleString("en") + " km2"
+      );
 
-    $("#flag").empty();
-    const flagURL = response[0].flag;
-    const img = $("<img>");
-    img.addClass("flagImg");
-    img.attr("src", flagURL);
-    $("#flag").append(img);
+      $("#domain").text("Domain : " + response[0].topLevelDomain);
+      $("#intlphone").text("Calling Code : " + "+" + response[0].callingCodes);
+      $("#timezone").text("Timezone : " + response[0].timezones[0]);
 
-    // geography
+      $("#flag").empty();
+      const flagURL = response[0].flag;
+      const img = $("<img>");
+      img.addClass("flagImg");
+      img.attr("src", flagURL);
+      $("#flag").append(img);
 
-    $("#region").text("Region : " + response[0].region);
-    $("#subregion").text("Subregion : " + response[0].subregion);
-    $("#borders").text(
-      "Bordering With : " + response[0].borders.length + " countries."
-    );
+      // geography
 
-    const countryLat = response[0].latlng[0];
-    const countryLng = response[0].latlng[1];
-    console.log(countryLat);
-    console.log(countryLng);
+      $("#region").text("Region : " + response[0].region);
+      $("#subregion").text("Subregion : " + response[0].subregion);
+      $("#borders").text(
+        "Bordering With : " + response[0].borders.length + " countries."
+      );
 
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: countryLat, lng: countryLng },
-      zoom: 3,
+      const countryLat = response[0].latlng[0];
+      const countryLng = response[0].latlng[1];
+
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: countryLat, lng: countryLng },
+        zoom: 3,
+      });
     });
-  });
+  $(".mainbox").removeClass("hide");
+  $("#userInput").val("");
 };
 
 $("#search").on("click", function (event) {
   event.preventDefault();
   searchCountry();
-  $("userInput").val("");
 });
